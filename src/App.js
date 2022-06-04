b4d1eed32e27a1f7bbcd3badc750d8042ae3c467
Ivan Borba <mrborbaivan@gmail.com>
2022-06-03 21:05:07 -0300

    feature: Dinâmica 2]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [list, setList] = useState([]);
+
+  const addToList = () => {
+    setList([...list, inputValue]);
+    setInputValue("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={inputValue}
+        onChange={(event) => {
+          setInputValue(event.target.value);
+        }}
+        placeholder="Digite o valor"
+      />
+      <button onClick={addToList}>Adicionar</button>
+      <ul>
+        {list.map((element, index) => {
+          return <li key={index}>{element}</li>;
+        })}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
+
+// Lista de passos pra criação da aplicação da dinâmica 2:
+// X Criar o projeto com o npx;
+// X Rodar o projeto em um terminal;
+// X Limpar o código inicial;
+// X Utilizar o useState para criar uma variavel de string para o input;
+// X Utilizar o useState para criar uma variavel de array para a lista;
+// X Criar a função que adicionar a string no array;
+// X Renderizar um botão para chamar a função;
+// X Renderizar o input e relacionar ele com o valor da string;
+// X Criar uma função para atualizar o valor da variavel, dependendo do valor do input;
+// X Renderizar a lista;
